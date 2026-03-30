f7ef5e6f17f4d11ba34b5db2155f6247268b25d5
Deivyson05 <deivysonrssantos@outlook.com>
2026-03-30 13:26:29 -0300

    feat: separa as atividades por conclusão e adiciona opção de desmarcar e deletar tarefas

--- expo-01/back4app/index.js
+++ expo-01/back4app/index.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const urlBase = "https://parseapi.back4app.com/classes/Tarefa";
 const headers = {
-  "X-Parse-Application-Id": "jnYc2snzzFZes5yr1TnS2l0ny7nB1Fyvlj8bq9wZ",
-  "X-Parse-JavaScript-Key": "iDYRd5O3Z2SnyfafWPneaqHiztKFSxFISMGqoBIY",
+  "X-Parse-Application-Id": "gtUStBBJcKj8YW8GZVg4fSvZd3Ya3Hb9giKMaDyo",
+  "X-Parse-JavaScript-Key": "drSgDDjEQkflViBBk8P0D19GmXY8Oi05kXKV6NQ0",
 };
 const headersJson = {
   ...headers,
@@ -14,6 +14,7 @@ export async function getTarefas() {
   const response = await axios.get(urlBase, {
     headers: headers,
   });
+  console.log("dados: ", response.data);
   return response.data.results;
 }
 
@@ -21,5 +22,38 @@ export async function adicionarTarefa(novaTarefa) {
   const response = await axios.post(urlBase, novaTarefa, {
     headers: headersJson,
   });
+  console.log("dados: ", response.data);
+  return response.data;
+}
+
+export async function concluirTarefa(id) {
+  const response = await axios.put(
+    `${urlBase}/${id}`,
+    { concluida: true },
+    {
+      headers: headersJson,
+    }
+  );
+  console.log("dados: ", response.data);
+  return response.data;
+}
+
+export async function excluirTarefa(id) {
+  const response = await axios.delete(`${urlBase}/${id}`, {
+    headers: headers,
+  });
+  console.log("dados: ", response.data);
+  return response.data;
+}
+
+export async function desmarcarTarefa(id) {
+  const response = await axios.put(
+    `${urlBase}/${id}`,
+    { concluida: false },
+    {
+      headers: headersJson,
+    }
+  );
+  console.log("dados: ", response.data);
   return response.data;
 }
